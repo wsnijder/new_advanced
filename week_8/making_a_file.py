70258675b130d45f12cbed2570efe66e15af8a49
Wijmar Snijder <snijderwijmar@gmail.com>
2020-11-19 14:38:27 +0100

    progress

---
+++ week_8/making_a_file.py
@@ -0,0 +1,6 @@
+from week_8.geometry.Point import Point
+
+point_p = Point(10, 5)
+
+#print("Point P coordinates are", point_p.x, point_p.y, point_p.distance_from_origin())
+print("Point P coordinates are", point_p)
